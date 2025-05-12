17459a6804cedca08ed2ced04fb28b51cf08dfa6
ArchieM-codes <archie.mclellan1@hotmail.com>
2025-05-12 19:15:29 +0100

    Update groups.js

--- messaging/groups/groups.js
+++ messaging/groups/groups.js
@@ -22,7 +22,7 @@ function generatePeerId() {
         return chars.charAt(Math.floor(Math.random() * chars.length));
     }).join('');
     const randomNumber2 = Math.floor(1000 + Math.random() * 9000);
-    return `AMC-${randomNumber1}-${randomLetters}-${randomNumber2}`;
+    return `AMCG-${randomNumber1}-${randomLetters}-${randomNumber2}`;
 }
 
 async function getOrCreatePeerId() {
@@ -44,9 +44,24 @@ function appendGroupMessage(message, sender) {
 }
 
 // Function to handle messages within the group
-function handleGroupMessage(e) {
+const handleGroupMessage = (e) => {
     console.log(e)
     appendGroupMessage(e.message, e.userID);
+};
+
+// Initialize or connect to a PeerGroup, depending on whether one already exists
+async function initializePeerGroup(groupId) {
+    if (peerGroupInstance) {
+        peerGroupInstance.removeEventListener("message", handleGroupMessage);
+        peerGroupInstance.disconnect(); // Disconnect from any existing group
+    }
+
+    //Now that the peer is connected, join the group
+    peerGroupInstance = new PeerGroup(peer);
+
+    peerGroupInstance.addEventListener("message", handleGroupMessage);
+
+    peerGroupInstance.connect(groupId, peer.id);
 }
 
 // Event listeners for group related actions
@@ -70,56 +85,6 @@ joinGroupBtn.addEventListener('click', async () => {
     initializePeerGroup(groupId);
 });
 
-// Initialize or connect to a PeerGroup, depending on whether one already exists
-async function initializePeerGroup(groupId) {
-    if (peerGroupInstance) {
-        peerGroupInstance.disconnect(); // Disconnect from any existing group
-    }
-
-    //Initialize the peer if it has not already
-    if(peer == null){
-        const peerId = await getOrCreatePeerId();
-        groupMyPeerId.innerText = peerId;
-        const peerConfig = {
-            host: '0.peerjs.com',
-            port: 443,
-            path: '/',
-            secure: true,
-            key: 'peerjs',
-            debug: 3,
-            config: {
-                iceServers: [{
-                    urls: ["stun:eu-turn4.xirsys.com"]
-                }, {
-                    username: "vXp0ehXgRlCJeYdQBR4hjAdVn42ttLfds4jTAVrRmD5RTceXb9qp-sCf1PEw5eWiAAAAAGggndthcmchiemtvc",
-                    credential: "fab9d62a-2e66-11f0-b4dc-0242ac140004",
-                    urls: [
-                        "turn:eu-turn4.xirsys.com:80?transport=udp",
-                        "turn:eu-turn4.xirsys.com:3478?transport=udp",
-                        "turn:eu-turn4.xirsys.com:80?transport=tcp",
-                        "turn:eu-turn4.xirsys.com:3478?transport=tcp",
-                        "turns:eu-turn4.xirsys.com:443?transport=tcp",
-                        "turns:eu-turn4.xirsys.com:5349?transport=tcp"
-                    ]
-                }]
-            }
-        };
-
-        peer = new Peer(peerId, peerConfig);
-        peer.on('open', function (id) {
-            console.log("Peer object on 'open':", peer);
-            myUserID = id;
-        });
-    }
-
-    // Now that the peer is connected, join the group
-    peerGroupInstance = new PeerGroup(peer);
-    peerGroupInstance.connect(groupId, peer.id);
-
-    //Register group functions
-    peerGroupInstance.addEventListener("message", handleGroupMessage);
-}
-
 // Event listener for sending group messages
 sendGroupMessageBtn.addEventListener('click', () => {
     const message = groupMessageInput.value;
@@ -130,8 +95,39 @@ sendGroupMessageBtn.addEventListener('click', () => {
     }
 });
 
-// Load the group ID when the document is ready
-document.addEventListener('DOMContentLoaded', async function () {
+// Run when the window is ready
+window.onload = async function () {
+    //Initialize the peer
     const peerId = await getOrCreatePeerId();
     groupMyPeerId.innerText = peerId;
-});
+    const peerConfig = {
+        host: '0.peerjs.com',
+        port: 443,
+        path: '/',
+        secure: true,
+        key: 'peerjs',
+        debug: 3,
+        config: {
+            iceServers: [{
+                urls: ["stun:eu-turn4.xirsys.com"]
+            }, {
+                username: "vXp0ehXgRlCJeYdQBR4hjAdVn42ttLfds4jTAVrRmD5RTceXb9qp-sCf1PEw5eWiAAAAAGggndthcmchiemtvc",
+                credential: "fab9d62a-2e66-11f0-b4dc-0242ac140004",
+                urls: [
+                    "turn:eu-turn4.xirsys.com:80?transport=udp",
+                    "turn:eu-turn4.xirsys.com:3478?transport=udp",
+                    "turn:eu-turn4.xirsys.com:80?transport=tcp",
+                    "turn:eu-turn4.xirsys.com:3478?transport=tcp",
+                    "turns:eu-turn4.xirsys.com:443?transport=tcp",
+                    "turns:eu-turn4.xirsys.com:5349?transport=tcp"
+                ]
+            }]
+        }
+    };
+
+    peer = new Peer(peerId, peerConfig);
+    peer.on('open', function (id) {
+        console.log("Peer object on 'open':", peer);
+        myUserID = id;
+    });
+};
